c1ca7627cd5425964cd8fa3d1e0a3e3eaa955cc5
Danilo Ascione <ascione@strands.com>
2016-06-19 11:35:34 +0200

    Add script based on benchmark script with some feature engineering testing

---
+++ script.R
@@ -0,0 +1,72 @@
+# This script trains a Random Forest model based on the data,
+# saves a sample submission, and plots the relative importance
+# of the variables in making predictions
+
+# Download 1_random_forest_r_submission.csv from the output below
+# and submit it through https://www.kaggle.com/c/titanic-gettingStarted/submissions/attach
+# to enter this getting started competition!
+
+library(ggplot2)
+library(randomForest)
+
+set.seed(1)
+train <- read.csv("./datasets/train.csv", stringsAsFactors=FALSE)
+test  <- read.csv("./datasets/test.csv",  stringsAsFactors=FALSE)
+
+test$Survived <- NA
+
+#Group all data
+all_data <- rbind(train, test)
+
+# How to fill in missing Age values?
+# We make a prediction of a passengers Age using the other variables and a decision tree model. 
+# This time you give method = "anova" since you are predicting a continuous variable.
+library(rpart)
+library("rattle")
+library("rpart.plot")
+library("RColorBrewer")
+predicted_age <- rpart(Age ~ Pclass + Sex + SibSp + Parch + Fare + Embarked,
+                       data = all_data[!is.na(all_data$Age),], method = "anova")
+all_data$Age[is.na(all_data$Age)] <- predict(predicted_age, all_data[is.na(all_data$Age),])
+
+# Split the data back into a train set and a test set
+train <- all_data[1:891,]
+test <- all_data[892:1309,]
+
+extractFeatures <- function(data) {
+  features <- c("Pclass",
+                "Age",
+                "Sex",
+                "Parch",
+                "SibSp",
+                "Fare",
+                "Embarked")
+  fea <- data[,features]
+  fea$Age[is.na(fea$Age)] <- -1
+  fea$Fare[is.na(fea$Fare)] <- median(fea$Fare, na.rm=TRUE)
+  fea$Embarked[fea$Embarked==""] = "S"
+  fea$Sex      <- as.factor(fea$Sex)
+  fea$Embarked <- as.factor(fea$Embarked)
+  fea$Family <- fea$Parch + fea$SibSp + 1
+  return(fea)
+}
+
+
+rf <- randomForest(extractFeatures(train), as.factor(train$Survived), ntree=1000, importance=TRUE)
+
+submission <- data.frame(PassengerId = test$PassengerId)
+submission$Survived <- predict(rf, extractFeatures(test))
+write.csv(submission, file = "submission.csv", row.names=FALSE)
+
+imp <- importance(rf, type=1)
+featureImportance <- data.frame(Feature=row.names(imp), Importance=imp[,1])
+
+ggplot(featureImportance, aes(x=reorder(Feature, Importance), y=Importance)) +
+     geom_bar(stat="identity", fill="green") +
+     coord_flip() + 
+     theme_light(base_size=20) +
+     xlab("") +
+     ylab("Importance") + 
+     ggtitle("Random Forest Feature Importance\n") +
+     theme(plot.title=element_text(size=18))
+
