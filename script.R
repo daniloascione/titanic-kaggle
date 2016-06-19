6649b91cbbe3e62a4bf67295f970e1ca11077720
Danilo Ascione <ascione@strands.com>
2016-06-19 15:00:13 +0200

    Add Fare per person feature

--- script.R
+++ script.R
@@ -18,6 +18,14 @@ test$Survived <- NA
 #Group all data
 all_data <- rbind(train, test)
 
+features <- all_data
+features$Fare[is.na(features$Fare)] <- median(features$Fare, na.rm=TRUE)
+features$Embarked[features$Embarked==""] = "S"
+features$Sex      <- as.factor(features$Sex)
+features$Embarked <- as.factor(features$Embarked)
+features$Family <- features$Parch + features$SibSp + 1
+features$Fare_pp <- features$Fare / features$Family
+
 # How to fill in missing Age values?
 # We make a prediction of a passengers Age using the other variables and a decision tree model. 
 # This time you give method = "anova" since you are predicting a continuous variable.
@@ -25,37 +33,22 @@ library(rpart)
 library("rattle")
 library("rpart.plot")
 library("RColorBrewer")
-predicted_age <- rpart(Age ~ Pclass + Sex + SibSp + Parch + Fare + Embarked,
-                       data = all_data[!is.na(all_data$Age),], method = "anova")
-all_data$Age[is.na(all_data$Age)] <- predict(predicted_age, all_data[is.na(all_data$Age),])
+predicted_age <- rpart(Age ~ Pclass + Sex + SibSp + Parch,
+                       data = features[!is.na(features$Age),], method = "anova")
+fancyRpartPlot(predicted_age)
+
+features$Age[is.na(features$Age)] <- predict(predicted_age, features[is.na(features$Age),])
 
 # Split the data back into a train set and a test set
-train <- all_data[1:891,]
-test <- all_data[892:1309,]
-
-extractFeatures <- function(data) {
-  features <- c("Pclass",
-                "Age",
-                "Sex",
-                "Parch",
-                "SibSp",
-                "Fare",
-                "Embarked")
-  fea <- data[,features]
-  fea$Age[is.na(fea$Age)] <- -1
-  fea$Fare[is.na(fea$Fare)] <- median(fea$Fare, na.rm=TRUE)
-  fea$Embarked[fea$Embarked==""] = "S"
-  fea$Sex      <- as.factor(fea$Sex)
-  fea$Embarked <- as.factor(fea$Embarked)
-  fea$Family <- fea$Parch + fea$SibSp + 1
-  return(fea)
-}
+train <- features[1:891,]
+test <- features[892:1309,]
 
 
-rf <- randomForest(extractFeatures(train), as.factor(train$Survived), ntree=1000, importance=TRUE)
+rf <- randomForest(as.factor(Survived) ~ Pclass + Sex + Age + SibSp + Parch + Fare_pp + Family, data = train, 
+                   importance = TRUE, ntree = 1000)
 
 submission <- data.frame(PassengerId = test$PassengerId)
-submission$Survived <- predict(rf, extractFeatures(test))
+submission$Survived <- predict(rf, test)
 write.csv(submission, file = "submission.csv", row.names=FALSE)
 
 imp <- importance(rf, type=1)
